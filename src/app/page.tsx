36edc116ca28d4e73f5d249b7aa6173af4dc2635
MateusPereirah <mateus.pereira1@outlook.com.br>
2025-05-29 15:09:29 -0300

    chore: add shadcn and tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div className="">Home</div>;
+  return (
+    <div className="">
+      <Button className="bg-amber-600">Bootcamp</Button>
+    </div>
+  );
 }
